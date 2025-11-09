a18ad9e7fac4030b8372d950fdb269d6e5f62b0c
SatinAu-Zelynn <mifanz090820@outlook.com>
2025-11-09 11:05:25 +0800

    docs: 更新站点状态链接的显示方式
    将站点状态的iframe替换为SVG图标和链接，以提供更好的用户体验
    - 移除了iframe元素，并将其替换为SVG图标和带有链接的文本“站点状态”
    - 调整了SVG图标的样式，使其垂直对齐并向上微移
    - 保持了版权信息的显示方式不变

--- script/components/footer.js
+++ script/components/footer.js
@@ -45,8 +45,13 @@ class SiteFooter extends HTMLElement {
       </style>
       <footer>
         <p>
-            <span id="build-info"style="vertical-align: middle"></span>
-            <iframe src="https://status.satinau.cn/badge?theme=light" width="250" height="30" frameborder="0" scrolling="no" style="color-scheme: normal; vertical-align: middle; margin-right: -80px;"></iframe>
+            <span id="build-info"></span>
+            <span class="footer-links">
+              <a href="https://status.satinau.cn" target="_blank">
+                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" style="transform: translateY(2px);" xmlns="http://www.w3.org/2000/svg"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 4m0 1a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1z"></path><path d="M7 20h10"></path><path d="M9 16v4"></path><path d="M15 16v4"></path><path d="M7 10h2l2 3l2 -6l1 3h3"></path></svg>
+                站点状态
+              </a>
+            </span>
             <br>
             © <span id="current-year"></span> 缎金SatinAu. 保留所有权利
             <span class="footer-links">
