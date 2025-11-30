4744bb1b0df7d065ee9f0bdb1642b2a3aab12156
SatinAu-Zelynn <mifanz090820@outlook.com>
2025-11-30 15:40:26 +0800

    feat(pages): 添加用户协议与隐私说明页面
    在用户协议与隐私说明页面中，新增了详细的用户协议、隐私政策及开源致谢内容。页面包括以下部分：
    - 版权声明和页面描述
    - 用户协议与隐私说明的标题和描述
    - 具体的数据跨境传输声明，包括使用 Cloudflare、Supabase/AWS 和 Microsoft Clarity 的说明
    - 第三方链接和责任边界声明
    - 嵌入式内容声明
    - 开源许可与致谢信息
    - 协议变更的说明
    
    此外，在设置页面中添加了一个导航项，链接到新创建的用户协议与隐私说明页面，并更新了页脚中的链接地址，将“开源协议”链接指向新创建的用户协议页面。

--- src/script/components/footer.js
+++ src/script/components/footer.js
@@ -53,9 +53,9 @@ class SiteFooter extends HTMLElement {
             <br>
             <span style="font-family: MiSans, sans-serif;">©</span> <span id="current-year"></span> 缎金SatinAu. 保留所有权利
             <span class="footer-links">
-              <a href="https://github.com/SatinAu-Zelynn/SatinAu-Website/blob/main/LICENSE" target="_blank">
+              <a href="/pages/agreement.html" target="_blank">
                 <svg class="footer-icon-a" style="fill: none; stroke: currentColor;" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"/></svg>
-                开源协议
+                用户协议
               </a>
               <a href="/sitemap.xml" target="_blank">
                 <svg class="footer-icon-a" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M19.527 4.799c1.212 2.608.937 5.678-.405 8.173-1.101 2.047-2.744 3.74-4.098 5.614-.619.858-1.244 1.75-1.669 2.727-.141.325-.263.658-.383.992-.121.333-.224.673-.34 1.008-.109.314-.236.684-.627.687h-.007c-.466-.001-.579-.53-.695-.887-.284-.874-.581-1.713-1.019-2.525-.51-.944-1.145-1.817-1.79-2.671L19.527 4.799zM8.545 7.705l-3.959 4.707c.724 1.54 1.821 2.863 2.871 4.18.247.31.494.622.737.936l4.984-5.925-.029.01c-1.741.601-3.691-.291-4.392-1.987a3.377 3.377 0 0 1-.209-.716c-.063-.437-.077-.761-.004-1.198l.001-.007zM5.492 3.149l-.003.004c-1.947 2.466-2.281 5.88-1.117 8.77l4.785-5.689-.058-.05-3.607-3.035zM14.661.436l-3.838 4.563a.295.295 0 0 1 .027-.01c1.6-.551 3.403.15 4.22 1.626.176.319.323.683.377 1.045.068.446.085.773.012 1.22l-.003.016 3.836-4.561A8.382 8.382 0 0 0 14.67.439l-.009-.003zM9.466 5.868L14.162.285l-.047-.012A8.31 8.31 0 0 0 11.986 0a8.439 8.439 0 0 0-6.169 2.766l-.016.018 3.665 3.084z"/></svg>
